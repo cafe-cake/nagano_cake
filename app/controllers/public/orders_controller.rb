8a912538423621a8fa2eec4415cc93b211bc2e7b
sakata shihori <rirakkuma.250@gmail.com>
2022-01-19 08:01:39 +0000

    [Add]order history　view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,4 +3,14 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @item_order = ItemOder.find(params[:id])
+  end
+
 end
