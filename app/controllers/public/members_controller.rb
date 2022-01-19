1d67d2cdaf3489fd94f8e13c7cd97a8dd3d05378
issa0133 <19950625issa@gmail.com>
2022-01-19 05:07:25 +0000

    member-info-edit-show

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,2 +1,29 @@
 class Public::MembersController < ApplicationController
+  def show
+    @member = Member.find(params[:id])
+  end
+  
+  def edit
+    @member = Member.find(params[:id])
+  end
+  
+  def update
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    redirect_to member_path(@member)
+  end
+  
+  def unsubscribe
+    
+  end
+  
+  def withdraw
+  end
+  
+  private
+  
+  def member_params
+  params.require(:member).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:post_number,:address,:phone_number,:email,:is_deleted)
+  end 
+
 end
