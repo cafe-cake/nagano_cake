ab732968de3be0667de4342e6d885d3d595e96c8
YK854 <95330893+YK854@users.noreply.github.com>
2022-01-20 13:29:30 +0900

    Delete 20220119143516_add_columns_to_item_orders.rb

--- db/migrate/20220119143516_add_columns_to_item_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToItemOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_column :item_orders, :order_id, :integer
-    add_column :item_orders, :item_id, :integer
-  end
-end
