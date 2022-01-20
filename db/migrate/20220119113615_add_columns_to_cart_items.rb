d4812a675840d25b97d69bf46c960d916251997e
YK854 <95330893+YK854@users.noreply.github.com>
2022-01-20 13:28:49 +0900

    Delete 20220119113615_add_columns_to_cart_items.rb

--- db/migrate/20220119113615_add_columns_to_cart_items.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToCartItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :cart_items, :member_id, :integer
-    add_column :cart_items, :item_id, :integer
-  end
-end
