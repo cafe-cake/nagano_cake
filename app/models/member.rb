841889b4e094f1ff47373f9029e6a75e0029aa72
issa0133 <19950625issa@gmail.com>
2022-01-20 08:53:01 +0000

    退会フラグの修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,12 +7,16 @@ class Member < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
+
   validates :is_deleted, inclusion: { in: [true, false] }
-  validates :last_name,:first_name,:address, presence: true,
+  validates :last_name,:first_name, presence: true,
     length: {in: 1..20 }
   validates :last_name_kana,:first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ , message: "カタカナで入力してください。"},
     length: {in: 1..20 }
+  validates :address, presence: true
   validates :post_number, format: {with: /\A\d{7}\z/ , message: "ハイフンなしの７桁で入力してください。"}
+  validates :phone_number, format: {with: /\A\d{10,11}\z/ , message: "ハイフンなしで入力してください。"}
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
