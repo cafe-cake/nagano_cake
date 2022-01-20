e9d5eb50d01ed9dc6e78990370430e25c31b1d19
YK854 <95330893+YK854@users.noreply.github.com>
2022-01-20 04:22:33 +0000

    ADD

---
+++ db/migrate/20220120041919_add_member_id_to_addresses.rb
@@ -0,0 +1,6 @@
+class AddMemberIdToAddresses < ActiveRecord::Migration[5.2]
+
+  def change
+    add_column :addresses, :member_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_152045) do
+ActiveRecord::Schema.define(version: 2022_01_20_041919) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_01_19_152045) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "member_id"
   end
 
   create_table "admins", force: :cascade do |t|
